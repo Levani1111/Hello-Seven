984f3266f1daf9390d08d5c563ed5135aca747a9
Levani1111 <papashvililevani@gmail.com>
2022-01-19 23:37:44 -0500

    post type & taxonomy

--- theme/functions.php
+++ theme/functions.php
@@ -8,3 +8,37 @@ function redirect_if_ip_starts_with_77_29() {
     }
 }
 add_action('init', 'redirect_if_ip_starts_with_77_29');
+
+// custome post type for projects
+function projects_post_type() {
+    $args = array(
+        'labels' => array(
+            'name' => __( 'Projects' ),
+            'singular_name' => __( 'Project' )
+        ),
+        'hierarchical' => true,
+        'public' => true,
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-analytics',
+        'supports' => array( 'title', 'editor', 'thumbnail',  'custom-fields'),
+        'rewrite' => array('slug' => 'my-Projects'),
+    );
+    register_post_type( 'projects', $args);
+      
+}
+add_action( 'init', 'projects_post_type' );
+
+// taxonomy for project type
+function project_type_taxonomy() {
+    $args = array(
+        'labels' => array(
+            'name' => __( 'Project type' ),
+            'singular_name' => __( 'type' )
+        ),
+        'hierarchical' => true,
+        'public' => true,
+       
+    );
+    register_taxonomy( 'type', array('projects'), $args);
+}
+add_action( 'init', 'project_type_taxonomy' );
