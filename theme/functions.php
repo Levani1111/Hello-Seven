b240b9519b786596d0012b880502a90602e93927
Levani1111 <papashvililevani@gmail.com>
2022-01-20 14:10:34 -0500

    upadte

--- theme/functions.php
+++ theme/functions.php
@@ -1,5 +1,23 @@
 <?php
 
+// load stylesheets
+function hello_seven_styles() {
+    wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all' );
+    wp_enqueue_style( 'bootstrap' );
+
+    // load main stylesheet
+    wp_enqueue_style( 'main', get_template_directory_uri() . '/css/main.css', array(), false, 'all' );
+    wp_enqueue_style( 'main' );
+}
+add_action( 'wp_enqueue_scripts', 'hello_seven_styles' );
+
+// load javascript
+function hello_seven_js() {
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), false, true );
+    wp_enqueue_script( 'bootstrap' );
+}
+add_action( 'wp_enqueue_scripts', 'hello_seven_js' );
+
 // Redirect function for ip address starting with 77.29
 function redirect_if_ip_starts_with_77_29() {
     if (strpos($_SERVER['REMOTE_ADDR'], '77.29') === 0) {
@@ -20,7 +38,7 @@ function projects_post_type() {
         'public' => true,
         'has_archive' => true,
         'menu_icon' => 'dashicons-analytics',
-        'supports' => array( 'title', 'editor', 'thumbnail',  'custom-fields'),
+        'supports' => array( 'title', 'editor', 'thumbnail'),
         'rewrite' => array('slug' => 'my-Projects'),
     );
     register_post_type( 'projects', $args);
@@ -32,13 +50,33 @@ add_action( 'init', 'projects_post_type' );
 function project_type_taxonomy() {
     $args = array(
         'labels' => array(
-            'name' => __( 'Project type' ),
+            'name' => __( 'Project types' ),
             'singular_name' => __( 'type' )
         ),
         'hierarchical' => true,
         'public' => true,
        
     );
-    register_taxonomy( 'type', array('projects'), $args);
+    register_taxonomy( 'types', array('projects'), $args);
 }
 add_action( 'init', 'project_type_taxonomy' );
+
+
+// register menus
+function register_theme_menus() {
+    register_nav_menus(
+        array(
+            'primary-menu' => __( 'Primary Menu' ),
+            'footer-menu' => __( 'Footer Menu' )
+        )
+    );
+}
+add_action( 'init', 'register_theme_menus' );
+
+/**
+ * Register Custom Navigation Walker
+ */
+function register_navwalker(){
+	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+add_action( 'after_setup_theme', 'register_navwalker' );
--- theme/header.php
+++ theme/header.php
@@ -12,3 +12,39 @@
 
 <body <?php body_class();?>>
     <?php wp_body_open(); ?>
+
+<headre>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mx-4" id="navbarSupportedContent">
+            <?php 
+                wp_nav_menu(array(
+                    'theme_location' => 'primary-menu',
+                    'container' => '',
+                    'container_class' => 'collapse navbar-collapse',
+                    'container_id' => '',
+                    'menu_class' => 'navbar-nav me-auto mb-2 mb-lg-0"',
+                    'menu_id' => '',
+                    'echo' => true,
+                    'fallback_cb' => 'wp_page_menu',
+                    'before' => '',
+                    'after' => '',
+                    'link_before' => '',
+                    'link_after' => '',
+                    'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                    'depth' => 0,
+                    'walker'            => new WP_Bootstrap_Navwalker(),
+
+                ));
+            ?>
+                <form class="d-flex" action="<?= get_home_url();?>">
+                    <input  name="s" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</headre>
